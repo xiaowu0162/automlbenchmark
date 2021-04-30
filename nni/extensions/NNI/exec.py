f967c63be8393a37f6224e227a50df6fe6487ffa
Di Wu <xiaowu200031@gmail.com>
2021-04-30 17:23:23 +0900

    Refactored arch selection code

--- nni/extensions/NNI/exec.py
+++ nni/extensions/NNI/exec.py
@@ -1,7 +1,7 @@
 import logging
 
 from .get_tuner import *
-from .run_random_forest import *
+from .run_experiment import *
 
 from amlb.benchmark import TaskConfig
 from amlb.data import Dataset
@@ -13,17 +13,10 @@ log = logging.getLogger(__name__)
 
     
 def run(dataset: Dataset, config: TaskConfig):
-    if 'arch_type' not in config.framework_params:
-        raise RuntimeError('framework.yaml does not have a "arch_type" field.')
-
     tuner, description = get_tuner(config)
     log.info("Tuning {} with NNI {} with a maximum time of {}s\n"
              .format(config.framework_params['arch_type'], description, config.max_runtime_seconds))
-    if config.framework_params['arch_type'] == 'random forest':
-        probabilities, predictions, train_timer, y_test = run_random_forest(dataset, config, tuner, log)
-    else:
-        raise RuntimeError('The requested arch type in framework.yaml is unavailable.')
-    
+    probabilities, predictions, train_timer, y_test = run_experiment(dataset, config, tuner, log)
     
     save_predictions_to_file(dataset=dataset,
                              output_file=config.output_predictions_file,
